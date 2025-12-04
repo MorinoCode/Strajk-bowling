12eacfc505a7df58ccfede99c5c97ce812770492
MorinoCode <mortezamastercode@gmail.com>
2025-12-04 13:48:15 +0100

    fix: correct setupTests path in vitest config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./setupTests.js",
+    setupFiles: "./src/setupTests.js",
 
     include: ["src/**/*.test.jsx"],
 
