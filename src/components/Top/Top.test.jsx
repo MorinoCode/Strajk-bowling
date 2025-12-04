61251448b3512982feb00627f86595435349ec49
MorinoCode <mortezamastercode@gmail.com>
2025-12-04 16:05:23 +0100

    fix: exclude router and App from vitest environment

--- src/components/Top/Top.test.jsx
+++ src/components/Top/Top.test.jsx
@@ -1,14 +1,9 @@
 import { render, screen } from "@testing-library/react";
 import Top from "./Top";
-import { MemoryRouter } from "react-router-dom";
 
 describe("Top Component", () => {
   it("renders logo image and heading", () => {
-    render(
-      <MemoryRouter>
-        <Top title="BOOKING" />
-      </MemoryRouter>
-    );
+    render(<Top title="BOOKING" />);
 
     const img = screen.getByRole("img");
     const heading = screen.getByRole("heading", { level: 1 });
