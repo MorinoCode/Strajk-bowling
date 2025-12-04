1d6bab387b26ddc4f69fdd57dd23753975817249
MorinoCode <mortezamastercode@gmail.com>
2025-12-04 16:09:25 +0100

    fix: exclude router and App from vitest environment

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
-    globals: true,
     environment: "jsdom",
     setupFiles: "./src/setupTests.js",
-
-
-    include: ["src/**/*.test.jsx"],
-
-    exclude: [
-      "src/main.jsx",
-      "src/App.jsx",
-      "src/router.jsx",
-      "src/**/*.scss",
-      "src/**/*.svg",
-    ],
+    globals: true,
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html"],
+      reportsDirectory: "coverage",
+    },
   },
 });
